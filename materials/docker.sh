138896033cae115e250941e9969feb2810bdd131
Robert Giessmann <rgiessmann@gmail.com>
2022-03-16 10:54:59 +0100

    sets up traffic branch

--- materials/docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#docker build -t asdf . && docker run --rm -it asdf
-docker build -t asdf . && docker run --rm -it asdf python ./experimentation-wedding-problem.py
-#docker build -t asdf . && docker run --rm -it asdf python ./wedding_initial.py
